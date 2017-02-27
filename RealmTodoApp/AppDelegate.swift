823ee61cad9ce96fff3e7be19f8a55b23adf6b3f
Pedro Martín <pmargom@gmail.com>
2017-02-27 11:36:01 +0000

    Added Real Swift dependency using cocoa pods

--- RealmTodoApp/AppDelegate.swift
+++ RealmTodoApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow()
-        window?.backgroundColor = UIColor.lightGray()
+        window?.backgroundColor = UIColor.red
         window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
 
